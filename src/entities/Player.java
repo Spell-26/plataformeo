1646bf3fc072d9d665c4f1c1bc7a836331936955
viper2612 <jsc.2612@gmail.com>
2023-04-22 11:09:26 -0500

    Se movieron todos los meditos que daban animación al personaje que se encontraban en la clase gamePanel a esta, ahora el movimiento se hace mediante true y falses y se eliminaron los movimientos basados en deltax y deltay, ahora estos valores se envian a la clase padre entity

---
+++ src/entities/Player.java
@@ -0,0 +1,193 @@
+package entities;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+
+import static utils.Constants.Directions.*;
+import static utils.Constants.Directions.DOWN;
+import static utils.Constants.PlayerConstants.*;
+
+public class Player extends Entity {
+
+    private BufferedImage[][] animations;
+
+    private int animationTick, animationIndex, animationSpeed = 30; //120fps/6 animaciones por segundo = 20animation speed
+
+    //animaciones desde Constants
+    private int playerAction = IDLE_2;
+
+    //MOVIMIENTO
+
+    private boolean left, right, up, down;
+    private boolean moving = false, attacking = false, jumping = false;
+
+    private float playerSpeed = 2.0f;
+
+    public Player(float x, float y){
+        super(x, y);
+        loadAnimations();
+    }
+
+    public void update(){
+
+        //valida si hay cambios en el panel y actualiza la posicion de los elementos
+        updatePosition();
+
+        updateAnimationTick();
+
+        //validar input y asignar animacion correspondiente
+        setAnimation();
+
+
+    }
+
+    public void render(Graphics g){
+        g.drawImage(animations[playerAction][animationIndex], (int)x, (int)y, 200, 148, null);
+    }
+
+    //manejar el tiempo entre cada imagen para generar la animacion
+    private void updateAnimationTick() {
+        animationTick++;
+        if(animationTick >= animationSpeed){
+            animationTick = 0;
+            animationIndex++;
+            if(animationIndex >= GetSpriteAmount(playerAction)){
+                animationIndex = 0;
+                attacking = false;
+                jumping = false;
+            }
+        }
+    }
+
+    private void setAnimation() {
+
+        int startAnimation = playerAction;
+
+        if(moving){
+            playerAction = RUNNING;
+        }
+        else{
+            playerAction = IDLE_2;
+        }
+
+        if(attacking){
+            playerAction = ATTACK_1;
+        }
+
+        if(jumping){
+            playerAction = JUMP;
+        }
+
+        if(startAnimation != playerAction){
+            resetAnimationTick();
+        }
+    }
+
+    private void resetAnimationTick() {
+        animationTick = 0;
+        animationIndex = 0;
+    }
+
+    private void updatePosition() {
+
+        moving = false;
+
+        if(left && !right){
+            x -= playerSpeed;
+            moving = true;
+        }
+        else if(!left && right){
+            x += playerSpeed;
+            moving = true;
+        }
+
+        if(up && !down){
+            y -= playerSpeed;
+            moving = true;
+        }
+        else if(!up && down){
+            y += playerSpeed;
+            moving = true;
+        }
+    }
+
+    private void loadAnimations() {
+
+        InputStream is = getClass().getResourceAsStream("/heroe/heroe_sprites.png");
+
+        try {
+            BufferedImage img = ImageIO.read(is);
+
+            //asignamos un espacio en el array dependiendo de las imagenes que compongan nuestra animacion
+            animations = new BufferedImage[11][10];
+
+            for(int j = 0; j < animations.length; j++){
+                for(int i = 0; i < animations[j].length; i++){
+                    animations[j][i] = img.getSubimage(i*50,j*37,50,37);
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally{
+            try {
+                is.close();
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+
+
+    }
+
+    public void resetDirBooleans(){
+        left = false;
+        right = false;
+        up = false;
+        down = false;
+    }
+
+    //getters y setters para movimiento (up, down, left, right)
+
+    public void setAttack(boolean attacking){
+        this.attacking = attacking;
+    }
+
+    public void setJumping(boolean jumping){
+        this.jumping = jumping;
+    }
+
+    public boolean isLeft() {
+        return left;
+    }
+
+    public void setLeft(boolean left) {
+        this.left = left;
+    }
+
+    public boolean isRight() {
+        return right;
+    }
+
+    public void setRight(boolean right) {
+        this.right = right;
+    }
+
+    public boolean isUp() {
+        return up;
+    }
+
+    public void setUp(boolean up) {
+        this.up = up;
+    }
+
+    public boolean isDown() {
+        return down;
+    }
+
+    public void setDown(boolean down) {
+        this.down = down;
+    }
+}
