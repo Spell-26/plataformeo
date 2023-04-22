37c771ea0f326669cb506e757315253a91581fb1
viper2612 <jsc.2612@gmail.com>
2023-04-22 11:16:11 -0500

    Se le añadio el listener del foco de la ventana

--- src/main/GameWindow.java
+++ src/main/GameWindow.java
@@ -1,6 +1,9 @@
 package main;
 
 import javax.swing.*;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowFocusListener;
+import java.awt.event.WindowListener;
 
 public class GameWindow {
     private JFrame jframe;
@@ -28,5 +31,17 @@ public class GameWindow {
         jframe.setVisible(true);
         //el metodo setVisible() siempre debe ir al final del bloque de codigo
         // para evitar que ocurran bugs inexperados
+
+        jframe.addWindowFocusListener(new WindowFocusListener() {
+            @Override
+            public void windowGainedFocus(WindowEvent e) {
+
+            }
+
+            @Override
+            public void windowLostFocus(WindowEvent e) {
+                gamePanel.getGame().windowFocusLost();
+            }
+        });
     }
 }
